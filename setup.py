6da59806206ec90abcdb5bb7c41b50295deb55bd
Raphael Michel <mail@raphaelmichel.de>
2018-09-11 17:58:55 +0200

    Release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ cmdclass = {
 
 setup(
     name='pretix-sofort',
-    version='1.2.0',
+    version='1.3.0',
     description='pretix payment via Klarna Sofort',
     long_description=long_description,
     url='https://github.com/pretix/pretix-sofort',
