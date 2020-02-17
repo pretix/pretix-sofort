9a4eecf18306fc1c289143ffab604a98759ff8ac
Raphael Michel <mail@raphaelmichel.de>
2020-02-17 17:02:36 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ cmdclass = {
 
 setup(
     name='pretix-sofort',
-    version='1.3.2',
+    version='1.3.3',
     description='pretix payment via Klarna Sofort',
     long_description=long_description,
     url='https://github.com/pretix/pretix-sofort',
