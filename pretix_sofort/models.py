8d4600516bd7985cab6822ac3cb5eef042b29858
Raphael Michel <michel@rami.io>
2026-07-03 10:10:50 +0200

    Drop all functionality

--- pretix_sofort/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class ReferencedSofortTransaction(models.Model):
-    reference = models.CharField(max_length=190, db_index=True, unique=True)
-    order = models.ForeignKey("pretixbase.Order", on_delete=models.CASCADE)
-    payment = models.ForeignKey(
-        "pretixbase.OrderPayment", null=True, on_delete=models.CASCADE
-    )
--- pretix_sofort/signals.py
+++ pretix_sofort/signals.py
@@ -1,11 +1,8 @@
-import json
 from django.dispatch import receiver
-from django.template.loader import get_template
 from django.utils.translation import gettext_lazy as _
-from pretix.base.signals import (
-    logentry_display, register_payment_providers, requiredaction_display,
-)
 
+from pretix.base.signals import (
+    logentry_display, register_payment_providers, )
 from .payment import Sofort
 
 
@@ -30,19 +27,3 @@ def pretixcontrol_logentry_display(sender, logentry, **kwargs):
     return _("Sofort reported a status notification: {status}").format(
         status=status_codes.get(logentry.parsed_data.get("status"), "?")
     )
-
-
-@receiver(signal=requiredaction_display, dispatch_uid="sofort_requiredaction_display")
-def pretixcontrol_action_display(sender, action, request, **kwargs):
-    if not action.action_type.startswith("pretix_sofort"):
-        return
-
-    data = json.loads(action.data)
-
-    if action.action_type == "pretix_sofort.sofort.overpaid":
-        template = get_template("pretix_sofort/action_overpaid.html")
-    elif action.action_type == "pretix_sofort.sofort.refund":
-        template = get_template("pretix_sofort/action_refund.html")
-
-    ctx = {"data": data, "event": sender, "action": action}
-    return template.render(ctx, request)
